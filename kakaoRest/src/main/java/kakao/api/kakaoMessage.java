73afcd1a3b84156a12c3677dfbd63c53cf1189e0
Jisoo Jang <ppuagirls@naver.com>
2017-08-09 23:17:49 +0900

    2017-08-09 지수 kakaoMessage if문 분리

--- kakaoRest/src/main/java/kakao/api/kakaoMessage.java
+++ kakaoRest/src/main/java/kakao/api/kakaoMessage.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import kakao.controller.BusInfoController;
-import kakao.controller.RestaurantController;
-import kakao.controller.TrafficInfoController;
 import kakao.domain.Message;
-import kakao.enumT.FoodKind;
-import naver.api.Restaurant;
+import kakao.functionCall.BusInfo;
+import kakao.functionCall.RestaurantInfo;
+import kakao.functionCall.SubwayInfo;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
@@ -25,171 +22,55 @@ import net.sf.json.JSONObject;
 @RestController
 @RequestMapping("message")
 public class kakaoMessage {
-
-	static String RESTAURANT_FLOW;
-	Set<String> foodKind = FoodKind.containFoodKind();
-	private Restaurant restaurantInfo = new Restaurant();
-	
-	static int subway_button = 0;
-	static int bus_button = 0;
 	
 	static String check = null;
-	static String station_id;
-	String station_name;
 
 	@Autowired
-	private RestaurantController restaurant;
-
+	private BusInfo bus_info;
+	
 	@Autowired
-	private TrafficInfoController trafficInfo;
-
+	private SubwayInfo subway_info;
+	
 	@Autowired
-	private BusInfoController busInfo;
+	private RestaurantInfo restaurant_info;
+
 
 	@RequestMapping(method = RequestMethod.POST)
 	String returnMessage(@RequestBody Message message)  {
 
-
-		// 정민 start
-		/*if ("맛집추천".equals(message.getContent())) {
-			RESTAURANT_FLOW = "ONE";
-			return restaurant.restaurantLocationMessage();
-		}
-
-		if (RESTAURANT_FLOW.equals("ONE")) {
-			RESTAURANT_FLOW = "TWO";
-			restaurantInfo.setLocationName(message.getContent());
-			return restaurant.restaurantKindMessage();
-		}
-
-		if (RESTAURANT_FLOW.equals("TWO") && foodKind.contains(message.getContent())
-				&& !restaurantInfo.getLocationName().equals(null)) {
-			RESTAURANT_FLOW = "FALSE";
-			restaurantInfo.setKind(message.getContent());
-			return restaurant.restaurantApiMessage(restaurantInfo.getLocationName(), message.getContent());
-		}
-
-		if(RESTAURANT_FLOW.equals("TWO") && "기타".equals(message.getContent())
-				&& !restaurantInfo.getLocationName().equals(null)){
-			RESTAURANT_FLOW = "THREE";
-			return restaurant.restaurantEtcMessage();
-		}
-
-		if(RESTAURANT_FLOW.equals("THREE")){
-			RESTAURANT_FLOW = "FALSE";
-			restaurantInfo.setKind(message.getContent());
-			return restaurant.restaurantApiMessage(restaurantInfo.getLocationName(), message.getContent());
-		}
-
-
-		if (RESTAURANT_FLOW.equals("FALSE") && "이거 먹을래".equals(message.getContent())) {
-			return restaurant.restaurantEatMessage();
-		}
-
-		if (RESTAURANT_FLOW.equals("FALSE") && "다른거 추천해줘".equals(message.getContent())) {
-			return restaurant.restaurantApiMessage(restaurantInfo.getLocationName(), restaurantInfo.getKind());
-		}*/
-		// 정민 end
-
-
-		// 지수 담당 start
-		if(message.getContent().equals("지하철")) {	// 1) 지하철 버튼을 누른다
-			subway_button = 1;
-			return trafficInfo.press_subway_button();
-		}
-
-
-		if(subway_button == 1) {	// 2) 지하철역 이름을 입력한다
-			subway_button = 2;
-			station_name = message.getContent();
-			if(station_name.charAt(station_name.length()-1) == '역') {
-				station_name = station_name.substring(0, station_name.length()-1);
+		if (message.getContent().equals("지하철")) { 
+			check = "subway"; 
 			}
-			return trafficInfo.choose_subway_line(station_name);	// 입력한 지하철역의 호선을 버튼으로 전달
+		else if (message.getContent().equals("버스")) { 
+			check = "bus"; 
+			}
+		else if ( message.getContent().equals("맛집추천")) { 
+			check = "restr"; 
+			}
+		else if (message.getContent().equals("처음으로")) { 
+			check = null;
+			return getKeyboard();
+			}
+		else { 
+			
 		}
-
-		else if(subway_button == 2) {	// 3) 지하철 호선을 선택한다
-			subway_button = 3;
-			String station_line = message.getContent();
-			return trafficInfo.write_subway_name(station_name, station_line);
-		}
-
-		else if(subway_button == 3) {	//4) 다른 역 검색, 즐겨찾는 구간 추가, 처음으로 버튼 중 1개를 클릭한다
-			if(message.getContent().equals("다른 역 검색")) {
-				subway_button = 1;
-				return trafficInfo.press_subway_button();
+		
+		
+		if(check.equals("subway")) { 
+			return subway_info.getMessage(message); 
 			}
-			else if(message.getContent().equals("즐겨찾는 역 추가")) {
-
+		else if (check.equals("bus")) {
+			return bus_info.getMessage(message); 
 			}
-			else if(message.getContent().equals("처음으로")) {	// "처음으로" 버튼 클릭시
-				return getKeyboard();
+		else if (check.equals("restr")) {
+			return restaurant_info.getMessage(message); 
 			}
-		}
-
-
-
-
-		if(message.getContent().equals("버스")) {		//1) 버스 버튼을 누른다
-			bus_button = 1;
-			return busInfo.press_bus_button();
-		}
-
-		if(bus_button == 1) {	//2) 정류소번호나 정류소이름을 입력한다
-
-			String tmp = message.getContent();
-			String check = tmp.replaceAll("[0-9]", "");
-			if(check.length() == 0 && tmp.length() == 5) {	//입력값이 숫자이면 (정류소번호) (전부 숫자로이루어진 5글자이면)
-				station_id = tmp;
-				bus_button = 2;
-				return busInfo.getStationByUidItem(station_id);
-			}
-			else if(check.length() == 0 && tmp.length() != 5) {	//잘못된 입력값이면
-				return null;
-			}
-			else {		//입력값이 정류소이름이면 
-				bus_button = 3;
-				return busInfo.write_station_name(tmp);
-			}
-
-		}
-		else if(bus_button == 2) {
-			bus_button = 4;
-			String bus_num = message.getContent();
-			return busInfo.choose_bus_num(bus_num, station_id);
-		}
-		else if(bus_button == 3) {
-			bus_button = 2;
-			int index = message.getContent().indexOf('[');
-			station_id = message.getContent().substring(index + 1, index + 6);
-			return busInfo.getStationByUidItem(station_id); 
-		}
-		else if(bus_button == 4) {
-			String msg = message.getContent();
-			if(msg.equals("다른 버스 검색")) {
-				bus_button = 2;
-				return busInfo.getStationByUidItem(station_id); 
-			}
-			else if(msg.equals("다른 정류소 검색")) {
-				bus_button = 1;
-				return busInfo.press_bus_button();
-			}
-			else if(msg.equals("즐겨찾는 버스 추가")) {
-
-			}
-			else if(msg.equals("처음으로")) {	// 처음으로 버튼 클릭 시
-				return getKeyboard();
-			}
-		}
 		else {
-
-		}
-		// 지수 담당 end
-
-
-		return null;
+			return null; 
+			}
 	}
-
+	
+	
 	String getKeyboard() throws JSONException {
 
 		JSONObject jsonObject = new JSONObject();
---
+++ kakaoRest/src/main/java/kakao/functionCall/functionCallConfig.java
@@ -0,0 +1,8 @@
+package kakao.functionCall;
+
+import kakao.domain.Message;
+
+public interface functionCallConfig {
+
+	public String getMessage(Message message);
+}
